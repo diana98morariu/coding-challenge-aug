0f16865fbcc110756328a9ca8561c3b1d236e1eb
Diana Morariu <32464376+diana98morariu@users.noreply.github.com>
2021-08-15 14:32:05 +0200

    fetch and sort data

--- coding-challenge-diana/src/App.js
+++ coding-challenge-diana/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+// import "./styles.sass";
 
-function App() {
+const App = () => {
+  const topStoriesUrl = "https://hacker-news.firebaseio.com/v0/topstories.json";
+  const randomStories = [];
+
+  async function getAndSortData() {
+    const apiCallIds = await fetch(topStoriesUrl);
+    const ids = await apiCallIds.json();
+    if (ids) {
+      for (let i = 0; i < 10; i++) {
+        var num = Math.floor(Math.random() * ids.length);
+        const apiCallStories = await fetch(
+          `https://hacker-news.firebaseio.com/v0/item/${ids[num]}.json`
+        );
+        const story = await apiCallStories.json();
+        if (story) {
+          randomStories.push(story);
+          const apiCallUsers = await fetch(
+            `https://hacker-news.firebaseio.com/v0/user/${story.by}.json`
+          );
+          const user = await apiCallUsers.json();
+          if (user) {
+            randomStories[i].authorKarma = user.karma;
+          }
+        }
+      }
+    }
+    randomStories.sort((a, b) => (a.score > b.score ? 1 : -1));
+    console.log(randomStories);
+  }
+
+  useEffect(() => {
+    getAndSortData();
+  }, [randomStories]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
     </div>
   );
-}
-
+};
 export default App;
