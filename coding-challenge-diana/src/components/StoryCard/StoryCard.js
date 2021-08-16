16301a03fe049055879caa2c42f895d31bd19bd0
Diana Morariu <32464376+diana98morariu@users.noreply.github.com>
2021-08-16 13:02:19 +0200

    cleaning up

--- coding-challenge-diana/src/components/StoryCard/StoryCard.js
+++ coding-challenge-diana/src/components/StoryCard/StoryCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { FcLike, FcCalendar, FcManager, FcCloseUpMode } from "react-icons/fc";
 import { BsBoxArrowUpRight } from "react-icons/bs";
 import "./StoryCard.sass";
